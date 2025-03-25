e8e3fac16e84f77fd11ce9f7f2b0df55a849405a
Yudhishtran Elangovan <97242651+Yudhishtran@users.noreply.github.com>
2025-03-25 17:40:01 +0530

    Add files via upload
    understanding rolling updates working and rollbacks.

---
+++ rolling-updates-and-rollback-cli.sh
@@ -0,0 +1,13 @@
+kubectl create -f deployment-definition.yml
+
+kubectl get deployments
+
+kubectl apply -f deployment-definition.yml
+
+kubectl set image deployment/myapp-deploymeny nginx=nignx:1.9.1
+
+kubectl rollout status deployment/myapp-deployment
+
+kubectl rollout history deployment/myapp-deployment
+
+kubectl rollout undo deployment/myapp
